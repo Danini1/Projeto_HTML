31564398fbc26c898d4c7ffea9382b10fbdd6a3f
Danil <daniellx965@gmail.com>
2023-06-05 20:24:08 -0400

    feat: update composer.json

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'dani/selladministrator',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '4584b34ab8be1f407b08788816957ea5617f502c',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'dani/selladministrator' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '4584b34ab8be1f407b08788816957ea5617f502c',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
